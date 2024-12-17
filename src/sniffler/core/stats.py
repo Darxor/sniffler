776257531d5e61495e8d8478a7da3fbd10dbc022
Alexander Sevostianov <sevostianov@ecopsy.ru>
2024-12-17 04:35:57 +0300

    feat: add methods to retrieve top N largest images and documents by pages

--- src/sniffler/core/stats.py
+++ src/sniffler/core/stats.py
@@ -1,6 +1,8 @@
 from collections import Counter
+from pathlib import Path
 
 from .collector import Collection
+from ..researchers import ImageResearcher, LegacyOfficeResearcher, ModernOfficeResearcher, PdfResearcher
 
 
 class StatCalculator:
@@ -44,7 +46,10 @@ class StatCalculator:
                           values are the counts of files with those extensions.
         """
         cnt = Counter(str(file.get("extension", "no_extension")) for file in self.collection)
-        cnt["no_extension"] += cnt.pop("")
+        try:
+            cnt["no_extension"] += cnt.pop("")
+        except KeyError:
+            pass
         return cnt
 
     def top_n_largest_files(self, n: int) -> Collection:
@@ -58,3 +63,42 @@ class StatCalculator:
             Collection: A collection of the top N largest files, sorted by size in descending order.
         """
         return sorted(self.collection, key=lambda x: float(int(x.get("size", 0))), reverse=True)[:n]  # type: ignore
+
+    def top_n_largest_images(self, n: int) -> Collection:
+        """
+        Returns the top N largest images from the collection.
+
+        Args:
+            n (int): The number of largest images to return.
+
+        Returns:
+            Collection: A collection of the top N largest images, sorted by size in descending order.
+        """
+        images = [file for file in self.collection if ImageResearcher().accepts(Path(str(file.get("path"))))]
+
+        def get_area(file):
+            return float(file.get("width", 0)) * float(file.get("height", 0))
+
+        return Collection(sorted(images, key=get_area, reverse=True)[:n])
+
+    def top_n_documents_by_pages(self, n: int) -> Collection:
+        """
+        Returns
+        """
+
+        def get_page_count(file):
+            return int(file.get("page_count", 0))
+
+        def get_path(file):
+            return Path(file.get("path"))
+
+        documents = [
+            file
+            for file in self.collection
+            if any(
+                researcher.accepts(file=get_path(file))
+                for researcher in (PdfResearcher, ModernOfficeResearcher, LegacyOfficeResearcher)
+            )
+        ]
+
+        return Collection(sorted(documents, key=get_page_count, reverse=True)[:n])
